38025397215bc7d9ae086610d2e4a3c2eb43669c
Abhijeet Pawar <abhijeet@neovarsity.org>
2024-08-04 01:53:09 +0530

    fix lint errors

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -69,23 +69,23 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            I’m Abhijeet, a Berlin based software engineer.
+            I&apos;m Abhijeet, a Berlin based software engineer.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
-              I'm a software craftsman based in Berlin. I specialize in rapid prototyping and scaling web application and enjoy working with distributed systems.
+              I&apos;m a software craftsman based in Berlin. I specialize in rapid prototyping and scaling web application and enjoy working with distributed systems.
               I have an extensive experience with building products in the fintech & edtech space.
             </p>
             <p>
               Besides programming, I enjoy hiking, camping, nature & street photography, reading and talking business & philosophy. 
-              Currently, I'm also actively working on my fitness & focus on lifting and running as my primary fitness routines.
+              Currently, I&apos;m also actively working on my fitness & focus on lifting and running as my primary fitness routines.
             </p>
             <p>
               My favorite artforms are entreprenuership, cinema & nature. I enjoy meeting and talking to people who are doing anything creative in these domains. 
-              If this is you, I'd love to connect & hear from you!
+              If this is you, I&apos;d love to connect & hear from you!
             </p>
             <p>
-              Currently, I'm building <Link href="https://neovarsity.org" className='font-medium text-black dark:text-white'>Neovarsity</Link>, 
+              Currently, I&apos;m building <Link href="https://neovarsity.org" className='font-medium text-black dark:text-white'>Neovarsity</Link>, 
               an ed-tech venture solving for upskilling and reskilling of talent in the emerging deep tech industries.
             </p>
           </div>
